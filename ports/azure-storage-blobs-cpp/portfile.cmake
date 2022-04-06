15d20b3f367f44d1cae8f1a0ea9b8966a3acdec0
azure-sdk <azuresdk@microsoft.com>
2022-04-06 04:11:27 +0000

    2022-04-06: azure-storage-blobs-cpp 12.4.0

--- ports/azure-storage-blobs-cpp/portfile.cmake
+++ ports/azure-storage-blobs-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-storage-blobs_12.4.0-beta.1
-    SHA512 3f464cf0e03b001877001af944726c7106285de714c0cbc6b600ca0d5a6a405cc31f0cdb0a228a29400299a5d90fbf896f25cf9c413def2034c2a13a306b1b6c
+    REF azure-storage-blobs_12.4.0
+    SHA512 8d60970d0eff794cfcb6fb8471e7b0086cd8620072845195f67ae2fe2a7e1ecdaf21f10a80a708c981afa3589115f0fd2477a8c398d06b66b4babcd49f0c4c9a
 )
 
 vcpkg_cmake_configure(
