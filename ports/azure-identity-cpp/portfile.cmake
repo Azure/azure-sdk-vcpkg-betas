0d2131c32a40aeb90d37337f0d7f8e6f7d9977d1
azure-sdk <azuresdk@microsoft.com>
2022-06-07 23:31:28 +0000

    2022-06-07: azure-identity-cpp 1.3.0

--- ports/azure-identity-cpp/portfile.cmake
+++ ports/azure-identity-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-identity_1.3.0-beta.2
-    SHA512 8f5b2fecbb9e70d931ad3f766815dd5f09e801d1edd342a972b8914a55448fcf53cff1824b4fc571eae2a359d1eab18899da0804c2a533456fba2f24126f7fd4
+    REF azure-identity_1.3.0
+    SHA512 1ed1a16c86492971183cda7dc996c3704c525fde82f8ee87984416b45a5e003bb7a191ede0c3a33e21f6f09c3e324c44861006396dd72c830176a79c67b96bd9
 )
 
 vcpkg_cmake_configure(
