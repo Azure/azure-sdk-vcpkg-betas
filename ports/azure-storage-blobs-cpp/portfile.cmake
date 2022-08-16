ece99627d919f06fd9ed64aa9c2f675836c7b24e
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2022-08-16 16:51:56 -0700

    Fix azure-storage-blobs-cpp installation issue

--- ports/azure-storage-blobs-cpp/portfile.cmake
+++ ports/azure-storage-blobs-cpp/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
     REPO Azure/azure-sdk-for-cpp
     REF azure-storage-blobs_12.6.0-beta.1
     SHA512 73ea5c97ff3ce4088f293ca056c8883c43b29613905a0a8b1b8a0d097aff353a6f301d685b19458ba149131f4fbb187485a2f5e9e7e9759184c8838d839363a9
+    PATCHES
+        00101-fix_find_package.patch
 )
 
 vcpkg_cmake_configure(
