8cefe4fe97e9b11810b55dd8c6edd381276e2795
azure-sdk <azuresdk@microsoft.com>
2022-10-06 18:26:55 +0000

    2022-10-06: azure-core-cpp 1.8.0-beta.1

--- ports/azure-core-cpp/portfile.cmake
+++ ports/azure-core-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-core_1.7.2
-    SHA512 f24d02bf34f8be665e08a9a2799498f78daf6103a0ac5c10d79dc6b7b4461dac8ea2f91088e11bc90d0c74b59a59dc1e6361ad053718fb6da6a0691a9ac314a6
+    REF azure-core_1.8.0-beta.1
+    SHA512 02b445b8d8f2471b8a79eaa8b9999aabbc15ba2d787db571222c11998d85377374c481d1dea41b8480f862d4de65914def0bcbec8ea6b11a0ce955b392124402
 )
 
 vcpkg_check_features(
