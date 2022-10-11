4a04cab4d83e86a62a1ad120312bc944f35c5e1e
azure-sdk <azuresdk@microsoft.com>
2022-10-11 04:48:03 +0000

    2022-10-11: azure-storage-files-shares-cpp 12.3.0

--- ports/azure-storage-files-shares-cpp/portfile.cmake
+++ ports/azure-storage-files-shares-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-storage-files-shares_12.3.0-beta.1
-    SHA512 bae89ccced831c898dde841fe7361bf7a58251b3c604a0d08b069c2d1c46ce3e4e46acdf6b62e5a69fe992a6c4f453f6fcbadb5b8be4a7dde669b18f8321912b
+    REF azure-storage-files-shares_12.3.0
+    SHA512 baecf1dde71108491d29aa85276c73723a2afc7bbde0e78f62460a8b2fa065a25296239e8082cf75f744a7237a93fd5cba20541b437e2d32cb40599ba8fe722a
 )
 
 vcpkg_cmake_configure(
