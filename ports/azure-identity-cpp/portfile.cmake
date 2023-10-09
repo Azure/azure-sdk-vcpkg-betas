d086c9ea6fb82e2353ea97bdecd318359fcab3d4
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2023-10-09 11:50:29 -0700

    [azure-*-cpp] Shorten source paths

--- ports/azure-identity-cpp/portfile.cmake
+++ ports/azure-identity-cpp/portfile.cmake
@@ -5,8 +5,18 @@ vcpkg_from_github(
     SHA512 7325ccd04f3d29922f1a6cf7015035fd84c468fad40bbd5e5380e0e469e2dcdf877d26b1bbc571cc0289d8e3dcf08d2c281dbce53aab4a7d20b5cd9cab54df9a
 )
 
+if(EXISTS "${SOURCE_PATH}/sdk/identity/azure-identity")
+  file(REMOVE_RECURSE "${SOURCE_PATH}/sdk/identity/_")
+  file(REMOVE_RECURSE "${SOURCE_PATH}/sdk/_")
+  file(REMOVE_RECURSE "${SOURCE_PATH}/_")
+
+  file(RENAME "${SOURCE_PATH}/sdk/identity/azure-identity" "${SOURCE_PATH}/sdk/identity/_")
+  file(RENAME "${SOURCE_PATH}/sdk/identity" "${SOURCE_PATH}/sdk/_")
+  file(RENAME "${SOURCE_PATH}/sdk" "${SOURCE_PATH}/_")
+endif()
+
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}/sdk/identity/azure-identity/"
+    SOURCE_PATH "${SOURCE_PATH}/_/_/_"
     OPTIONS
         -DWARNINGS_AS_ERRORS=OFF
         -DBUILD_TESTING=OFF
