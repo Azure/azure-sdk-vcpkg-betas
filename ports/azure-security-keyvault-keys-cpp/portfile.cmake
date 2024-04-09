78a32bb48e85dc6a27c75a9a817c1ecb6b3b8cc8
azure-sdk <azuresdk@microsoft.com>
2024-04-09 21:03:30 +0000

    2024-04-09: azure-security-keyvault-keys-cpp 4.5.0-beta.1

--- ports/azure-security-keyvault-keys-cpp/portfile.cmake
+++ ports/azure-security-keyvault-keys-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-security-keyvault-keys_4.4.1
-    SHA512 758ff05ce33672d81cf14d347edada2a7c008c7df61639b7bac9ce1a1114144a1f6fcc3d364d1d90f5f9cda8f8c1db14f80873403d4190d4aca1844fb25dd517
+    REF azure-security-keyvault-keys_4.5.0-beta.1
+    SHA512 344dd3a683e35baa4554d7f9a9b74a5124bf146fe7491a23d1185bc6a9b989282373e033474afc3c8d782d2aabc6edac5399ce835976b7276c67820ffe2cc8ab
 )
 
 if(EXISTS "${SOURCE_PATH}/sdk/keyvault/azure-security-keyvault-keys")
