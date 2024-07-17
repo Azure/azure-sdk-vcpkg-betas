50814df3ba9ddf90c7d46817b2aa25d790eaccde
azure-sdk <azuresdk@microsoft.com>
2024-07-17 11:29:54 +0000

    2024-07-17: azure-storage-queues-cpp 12.3.0

--- ports/azure-storage-queues-cpp/portfile.cmake
+++ ports/azure-storage-queues-cpp/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
     REF "azure-storage-queues_${VERSION}"
-    SHA512 a3276c2a6de4ffe6daab8dd6ecf3e6cd3634e6cbb63c4d4ae06005cb337265ed5ba4d825f6336b0cb2ea3ff966d8ca1226e4e607ef0047a4b10921dee506006a
+    SHA512 1ee5d7214e56bce689c5c5d0f17fb95159469a1557b3cb1ad4b97e376b7036a2fc1c1178736b11c7003d17ae91fe77c0b140c335f7b4b823a14d858bb4a54300
     HEAD_REF main
 )
 
