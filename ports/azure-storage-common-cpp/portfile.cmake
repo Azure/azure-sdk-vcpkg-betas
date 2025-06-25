b2ef25f8f30b813c46db47d012936b0e50d988c4
azure-sdk <azuresdk@microsoft.com>
2025-06-25 06:29:28 +0000

    2025-06-25: azure-storage-common-cpp 12.11.0-beta.1

--- ports/azure-storage-common-cpp/portfile.cmake
+++ ports/azure-storage-common-cpp/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
     REF "azure-storage-common_${VERSION}"
-    SHA512 1bcb600b8f33ef09581996abf8a590a1c5f61cf5ac7ff52078cc15106df110df7dfe288bbc988d98ea526d526999646a68e2de53820a6ab85a0942ba3e7ad2e5
+    SHA512 67866cb080576ec5b01d227a61ac81cc4b5e5c0e46d75a5b16c84297b7bf76c94510df97456d412b13bde8ef1e3f416d0f0dc2801fc58f18904ed7f5708b149a
     HEAD_REF main
 )
 
